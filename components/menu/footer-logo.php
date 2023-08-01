67f0e304c5c83c9881d2be57a5af45e529a0c86b
Leandro OC <oliva1337@gmail.com>
2023-08-01 18:10:42 -0300

    Revert "ADVC-95 - Refatora Estrutura Footer p/ adaptação responsiva"
    This reverts commit 287983baed8fe5dbc161be7f482587e2e148e20a.

--- components/menu/footer-logo.php
+++ components/menu/footer-logo.php
@@ -5,5 +5,5 @@
     <a href="/" title="<?= !empty($campo->name) ? $campo->name : '' ?>" class="d-flex flex-column justify-content-center"><img src="<?= get_template_directory_uri() . "/assets/sedes/" . LANG . "/logo-iasd-vertical.svg" ?>" alt="<?= $campo->name ?>" title="<?= $campo->name ?>" class="img-fluid"></a>
     <span class="d-block mt-4"><?= !empty($campo->name) ? $campo->name : '' ?></span>
   </div>
-  <hr class="mt-2 mb-2 mt-lg-4 mb-lg-4">
+  <hr class="mt-4 mb-4">
 <?php endif; ?>
